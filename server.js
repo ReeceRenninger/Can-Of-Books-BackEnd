e5cd79262b17c4369c6bc9ac1ff254a8aa0dc3f4
ReeceRenninger <renningerreece@gmail.com>
2023-03-27 14:00:42 -0700

    proof of life running for local server

--- server.js
+++ server.js
@@ -3,16 +3,36 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { application } = require('express');
 
 const app = express();
+
+// Middleware
 app.use(cors());
 
+// Define PORT validate env is working
 const PORT = process.env.PORT || 3002;
 
+// Listen
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+// mongo url mongodb+srv://renningerreece:<password>@codingzone.didehis.mongodb.net/?retryWrites=true&w=majority
+
+
+//test route
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// Catch all error
+app.get('*', (request, response)=> {
+  response.status(404).send('Unavailable');
+});
+
+app.use((error, request, response, next)=>{
+  console.log(error.message);
+  response.status(500).send(error.message);
+});
